9c024dbd55a643a791f7b8dffffbda40450b1a7d
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-30 16:54:06 +0300

    Create child events to be handled by parent

---
+++ Router and route/src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="component">
+    <h1>This is my component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Router from components: ", this.$router);
+    console.log("Route from components: ", this.$route);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ Router and route/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>This is the home page</h1>
+    <Comp />
+  </div>
+</template>
+
+<script setup>
+import { useRouter, useRoute } from "vue-router";
+import Comp from "@/components/MyComponent.vue";
+
+const router = useRouter();
+console.log("Router from view: ", router);
+
+const route = useRoute();
+console.log("Route from view: ", route);
+</script>
