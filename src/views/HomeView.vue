0f73f9a6fae4be15f7fff5167702c8f4333b8083
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-30 14:11:00 +0300

    Use composition API functions to modify data dynamically

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
-    <h1>My name is {{ firstName }} {{ lastName }}</h1>
     <h3>Age is {{ age }}</h3>
-    <h4>User Active is {{ isActive }}</h4>
-    <h3>My brother that you see is {{ userObject.name }}</h3>
-    <h3>My favorite number is {{ myArray[0] }}</h3>
   </div>
+  <button @click="increaseAge">increase age</button>
 </template>
 
 <script setup>
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 
-const firstName = reactive("Mohanad");
-const lastName = ref("Altarah");
 const age = ref(24);
-const isActive = ref(true);
-const userObject = ref({
-  name: "Muhammed",
-  age: 30,
-});
-const myArray = ref([1, 2, 3, 4, 5]);
+
+function increaseAge() {
+  age.value++;
+}
 </script>
