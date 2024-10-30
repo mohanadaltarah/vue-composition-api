235ca7461b9392ae3f8fa0b29d6392f8a213fc6c
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-30 16:30:24 +0300

    Get and Set properties using defineProps

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="component">
+    <h1>This is my component</h1>
+    <h3>Name is: {{ props.userName }}</h3>
+    <h3>Age is: {{ props.userAge }}</h3>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  userName: {
+    type: String,
+    required: true,
+  },
+  userAge: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,15 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
-    <h1 ref="welcomeEl">{{ welcomeUser }}</h1>
-    <h1>{{ afterYears }}</h1>
+    <Comp :userAge="age" :userName="name" />
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
+import Comp from "@/components/MyComponent.vue";
 
 //Data
 const name = ref("Mohanad");
 const age = ref(24);
-const welcomeEl = ref("Mohanad");
-console.log(welcomeEl.value);
-setTimeout(() => {
-  console.log(welcomeEl.value);
-}, 2000);
-
-//Computed
-const welcomeUser = computed(
-  () => `Hello, ${name.value}, Your age is ${age.value}`
-);
-
-const afterYears = computed(
-  () =>
-    `Hey, ${name.value}, Your age will be ${age.value + 10} after ten years!`
-);
 </script>
