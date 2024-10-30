540aee5a3740a5b10ebdd816bb6bb90b74a35ea8
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-30 14:24:00 +0300

    Use the computed property to contain long and complex expressions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
-    <h3>Age is {{ age }}</h3>
+    <h1>{{ welcomeUser }}</h1>
+    <h1>{{ afterYears }}</h1>
   </div>
-  <button @click="increaseAge">increase age</button>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
+//Data
+const name = ref("Mohanad");
 const age = ref(24);
 
-function increaseAge() {
-  age.value++;
-}
+//Computed
+const welcomeUser = computed(
+  () => `Hello, ${name.value}, Your age is ${age.value}`
+);
+
+const afterYears = computed(
+  () =>
+    `Hey, ${name.value}, Your age will be ${age.value + 10} after ten years!`
+);
 </script>
