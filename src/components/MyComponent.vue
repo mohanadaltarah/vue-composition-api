9c435b958e8df79eb7ea8d8ef636d46bf3d9162a
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-30 16:40:08 +0300

    Declare router and route using the composition api

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,24 +1,16 @@
 <template>
   <div class="component">
     <h1>This is my component</h1>
-    <h3>Name is: {{ props.userName }}</h3>
-    <h3>Age is: {{ props.userAge }}</h3>
   </div>
 </template>
 
-<script setup>
-import { defineProps } from "vue";
-
-const props = defineProps({
-  userName: {
-    type: String,
-    required: true,
+<script>
+export default {
+  mounted() {
+    console.log("Router from components: ", this.$router);
+    console.log("Route from components: ", this.$route);
   },
-  userAge: {
-    type: Number,
-    required: true,
-  },
-});
+};
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
-    <Comp :userAge="age" :userName="name" />
+    <Comp />
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
 import Comp from "@/components/MyComponent.vue";
 
-//Data
-const name = ref("Mohanad");
-const age = ref(24);
+const router = useRouter();
+console.log("Router from view: ", router);
+
+const route = useRoute();
+console.log("Route from view: ", route.name);
 </script>
