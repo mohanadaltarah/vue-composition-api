3b252476e7be1edb068c5038c248ee295f9eb786
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-31 14:16:52 +0300

    Create advanced Event Emitts between components

---
+++ Event Bus/src/components/MyBtn.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="fireEmit">click me</button>
+</template>
+
+<script setup>
+import { inject } from "vue";
+
+const emitter = inject("emitter");
+
+const fireEmit = () => {
+  emitter.emit("globalEvent", "Mohanad");
+};
+</script>
+
+<style lang="scss" scoped></style>
