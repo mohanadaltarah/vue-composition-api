3fa28da1f361de67a125d5add729f426f862affd
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-31 13:54:12 +0300

    Create life cycle hooks using composition API

---
+++ Lifcycle Hooks/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component">
+    <h1>This is my component</h1>
+    <h3>Username is: {{ userName }}</h3>
+    <h3>Age is: {{ age }}</h3>
+    <button @click="sayHello">Say Hello</button>
+  </div>
+</template>
+
+<script setup>
+import { inject } from "vue";
+
+const userName = inject("userName");
+const age = inject("age");
+const sayHello = inject("sayHello");
+</script>
+
+<style lang="scss" scoped></style>
